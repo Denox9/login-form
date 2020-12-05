df6922e5047d2ef439fcf47c01d9657d83a454ea
Denox9 <kierwinramos28@gmail.com>
2020-12-05 13:27:00 +0800

    4 files modified

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Loginform } from "stackblitz:/src/app/login-form";
+
 @Component({
   selector: "app-login-form",
   templateUrl: "./login-form.component.html",
@@ -9,4 +11,7 @@ export class LoginFormComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  gender = ["Male", "Female"];
+
+  model = new Loginform();
 }
